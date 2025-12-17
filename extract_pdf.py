2baaa63962a5156829836b3bd419a86ccce52d19
juanmacanovas26 <juanmacanovas26@gmail.com>
2025-12-17 18:54:45 -0300

    Delete extract_pdf.py

--- extract_pdf.py
+++
@@ -1,8 +0,0 @@
-import pdfplumber
-
-with pdfplumber.open(r'c:\Users\juanm\Downloads\CV English 2025.pdf') as pdf:
-    text = ''
-    for page in pdf.pages:
-        text += page.extract_text() + '\n'
-
-print(text)
